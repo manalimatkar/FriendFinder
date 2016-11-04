7f0e4edd1447354815d4be0c83eba12c579e2ae6
Manali Matkar <Manali Matkar>
2016-11-04 11:06:35 -0500

    find best match done in post

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,6 @@
 // Export friends Data Object
-var friendArray = [{
+var friendArray = [
+{
   "name":"Ahmed",
   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
   "scores":[
@@ -9,5 +10,39 @@ var friendArray = [{
      4,
      5
     ]
-}];
+},
+{
+  "name":"Monica",
+  "photo":"http://monicahimmel.com/wp-content/themes/monica/img/picture.jpg",
+  "scores":[
+     3,
+     1,
+     3,
+     4,
+     2
+    ]
+},
+{
+  "name":"Jerry",
+  "photo":"http://blog.kevineikenberry.com/wp-content/uploads/2013/10/seinfeld_jerry.jpg",
+  "scores":[
+     1,
+     1,
+     2,
+     2,
+     5
+    ]
+},
+{
+  "name":"George",
+  "photo":"http://i.imgur.com/gzay9lw.png",
+  "scores":[
+     5,
+     4,
+     4,
+     3,
+     5
+    ]
+}
+];
 module.exports = friendArray;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -20,11 +20,54 @@ module.exports = function (app) {
 	app.post('/api/friends', function (req, res) {
 		// Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
 		// It will do this by sending out the value "true" have a table	
-			console.log(req.body);
-			friendData.push(req.body);
+			var diffArr = [];			
+			// friendData.push(req.body);
+			var currentUser = req.body;
+			var userScore = currentUser.scores;			
+			console.log(currentUser);
+			console.log(friendData);
 			// Logic to compare the datasubmitted by user to the data from friends data will go here
 			// return the data match in response instead of true
-			res.json(true); // KEY LINE
+
+			for (var i = 0; i < friendData.length ; i++) {				
+				var scoreArr = friendData[i].scores;
+				console.log(friendData[i].name);
+				var totalDifference = 0;
+				for(var j = 0 ; j < scoreArr.length; j++){
+					var diff = Math.abs(parseInt(userScore[j]) - parseInt(scoreArr[j]));
+					console.log(diff);	
+					totalDifference = totalDifference + diff;					
+				}
+				console.log("----------------------------------------------");
+				console.log("Difference Value " + totalDifference);
+				console.log("----------------------------------------------");
+				diffArr.push(totalDifference);
+			}
+
+			var lowestDiffIndex = indexOfSmallest(diffArr);
+
+			console.log(lowestDiffIndex);
+
+			function indexOfSmallest(a){
+				console.log("Inside indexoflowest :: " + a)
+				var lowest = 0;
+				for (var x = 1; x < a.length; x++) {
+				 if (a[x] < a[lowest])
+				 {
+				 	lowest = x;				 	
+				}
+				  
+				}
+				console.log("Inside indexoflowest :: " + lowest);
+				return lowest;
+			}
+
+				console.log("----------------------------------------------");
+				console.log("Best Matched :: " + friendData[lowestDiffIndex].name);
+				console.log("----------------------------------------------");
+
+
+			res.json(friendData[lowestDiffIndex]); // KEY LINE
 		
 	});
 };
